ab14d6251041b24fa884c57ec9e9285dc5a34742
Alon-M <along@moveo.co.il>
2025-07-21 20:40:07 +0300

    feat: changed home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './pages/not-found/not-found';
-import HomePage from './pages/home-page/home';
+import Home from './pages/home-page/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
